abacd1c212e7f36b4373a6e6dd3b745952bbfa7e
acantu9 <adrianaacantu@gmail.com>
2023-11-12 12:29:59 -0600

    defined columns

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,6 +7,16 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
