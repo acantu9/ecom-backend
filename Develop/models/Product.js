c12de8deaaf54c3ab4446f1a9c71748be7c3fecb
acantu9 <adrianaacantu@gmail.com>
2023-11-12 11:55:43 -0600

    defined columns

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,24 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id',
+      },
+      allowNull: false,
+    },
   },
   {
     sequelize,
